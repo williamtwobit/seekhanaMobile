ab87baac900b42166534376cc0029c06f9cfad7c
wilm <william@twobit.cc>
2017-09-25 19:28:01 -0400

    basic firbase connectivity happening!

--- App.js
+++ App.js
@@ -1,17 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+
+// firebase stuff ////
+import * as firebase from 'firebase';
+var config = {
+    apiKey: "AIzaSyCc8pKjxtgv_zZyVZLkyCDRjvCUER-ZHMA",
+    authDomain: "seekhana-cc86c.firebaseapp.com",
+    databaseURL: "https://seekhana-cc86c.firebaseio.com",
+    projectId: "seekhana-cc86c",
+    storageBucket: "seekhana-cc86c.appspot.com",
+    messagingSenderId: "498023864683"
+  };
+const fire = firebase.initializeApp(config);
+// //////// ////
+
+
+class Landing extends React.Component {
+  constructor(props){
+    super(props);
+    this.ref = fire.database().ref();
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount(){
+    this.ref.on('value', (snap) => {
+      let items = [];
+      snap.forEach(child => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        items: items
+      });
+
+    })
+  }
+
+  addNum(){
+    let i = Math.floor(Math.random() * 5);
+    const phrases = ['Shooby', 'lee', 'dooooo', 'doodle', 'pi', 'pie'];
+    let phrase = phrases[i];
+    this.ref.push({title: phrase});
+  }
+
+
   render() {
+    console.log(this.state.items);
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello!!</Text>
+        <Text>{this.state.items.map(item => {
+          return <Text key={item._key}>{item.title}</Text>
+        })}</Text>
+        <Button title="lets add a number" onPress={() => this.addNum()} />
       </View>
     );
   }
 }
+
+export default App = StackNavigator({
+  Landing: {screen: Landing},
+});
 
 const styles = StyleSheet.create({
   container: {
